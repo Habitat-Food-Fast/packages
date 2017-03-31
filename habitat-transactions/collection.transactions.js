a8d6ee9ef56c3b8517ed702b7f3fb340024321d7
jpmoyn <jpmoyn@gmail.com>
2017-03-31 14:22:45 -0400

    hello

--- habitat-transactions/collection.transactions.js
+++ habitat-transactions/collection.transactions.js
@@ -57,8 +57,7 @@ class transactionsCollection extends Mongo.Collection {
   forceInsert(txs) { return transactions.batchInsert(txs, (err) => { if(err) { throwError(err.message); } else { } }); }
   forceRemove() { return super.remove({}); }
   formatOrder(order, thirdParty){
-    console.log('yoooo');
-    console.log('hello');
+    console.log('HELLO');
     if(!thirdParty){
       return order.length === 0 ? order : order.map(order =>
          _.extend(order, {
