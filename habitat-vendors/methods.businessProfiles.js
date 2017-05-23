550197000dd266f64cb79ce17465cb0f2d5fd0bd
jpmoyn <jpmoyn@gmail.com>
2017-05-23 15:08:09 -0400

    moved some methods

--- habitat-vendors/methods.businessProfiles.js
+++ habitat-vendors/methods.businessProfiles.js
@@ -164,28 +164,53 @@ Meteor.methods({
     }
   },
   updateWeeklyHours (biz, myDay, field, val) {
-  if (!Roles.userIsInRole(Meteor.userId(), ['admin'])) { throw new Meteor.Error('unauthorized'); }
-  var weekArray = businessProfiles.findOne(biz).weeklyHours;
-  var hourObj = _.findWhere(weekArray, {day: myDay});
+    if (!Roles.userIsInRole(Meteor.userId(), ['admin'])) { throw new Meteor.Error('unauthorized'); }
+    var weekArray = businessProfiles.findOne(biz).weeklyHours;
+    var hourObj = _.findWhere(weekArray, {day: myDay});
 
-  if (field === 'open') {
-    businessProfiles.update({_id: biz, 'weeklyHours.day': myDay}, {$set: {
-      'weeklyHours.$.open': val}
+    if (field === 'open') {
+      businessProfiles.update({_id: biz, 'weeklyHours.day': myDay}, {$set: {
+        'weeklyHours.$.open': val}
+      });
+    } else {
+      var hour = Number(moment(val, 'h hh, a A').format('H'));
+      var min = Number(moment(val, 'HH:mm').format('m'));
+      var dayBase = myDay * 86400000;
+      var hourBase = hour * 3600000;
+      var minBase = min * 60000;
+      if (field === 'openHr') {
+        var openTime = dayBase + hourBase + minBase;
+        businessProfiles.update({_id: biz, 'weeklyHours.day': myDay}, {$set: {'weeklyHours.$.openTime': openTime, 'weeklyHours.$.openHr': val}});
+      }
+      if (field === 'closeHr') {
+        var closeTime = dayBase + hourBase + minBase;
+        businessProfiles.update({_id: biz, 'weeklyHours.day': myDay}, {$set: {'weeklyHours.$.closeTime': closeTime, 'weeklyHours.$.closeHr': val}});
+      }
+    }
+  },
+  changeBizState(id, type) {
+    if (Roles.userIsInRole(Meteor.userId(), ['admin'])) {
+      businessProfiles.update(id, {$set: {open: type}});
+    }
+  },
+  closeBusinessForToday (bizId) {
+    check(bizId, String);
+    if (Meteor.isServer) {
+      var now = Settings.findOne({name: 'weeklyMilliCount'}).count;
+      businessProfiles.update({_id: bizId, 'weeklyHours.day': moment().day()}, { $set: {'weeklyHours.$.quickClose': now}}, (err, res) => {
+        if (err) {
+          throw new Meteor.Error(err.reason);
+        }
+      });
+    }
+  },
+
+  resetTodaysBizHours (bizId) {
+    check(bizId, String);
+    businessProfiles.update({_id: bizId, 'weeklyHours.day': moment().day()}, { $unset: {'weeklyHours.$.quickClose': ''}}, (err, res) => {
+      if (err) {
+        throw new Meteor.Error(err.reason);
+      }
     });
-  } else {
-    var hour = Number(moment(val, 'h hh, a A').format('H'));
-    var min = Number(moment(val, 'HH:mm').format('m'));
-    var dayBase = myDay * 86400000;
-    var hourBase = hour * 3600000;
-    var minBase = min * 60000;
-    if (field === 'openHr') {
-      var openTime = dayBase + hourBase + minBase;
-      businessProfiles.update({_id: biz, 'weeklyHours.day': myDay}, {$set: {'weeklyHours.$.openTime': openTime, 'weeklyHours.$.openHr': val}});
-    }
-    if (field === 'closeHr') {
-      var closeTime = dayBase + hourBase + minBase;
-      businessProfiles.update({_id: biz, 'weeklyHours.day': myDay}, {$set: {'weeklyHours.$.closeTime': closeTime, 'weeklyHours.$.closeHr': val}});
-    }
   }
-},
 });
