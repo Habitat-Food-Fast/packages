a29cc5fc0d198b35a1631b15421a74aa0583c365
Alex <alex.du@tryhabitat.com>
2017-05-24 14:21:45 -0400

    'update'

--- habitat-transactions/methods.transactions.js
+++ habitat-transactions/methods.transactions.js
@@ -188,7 +188,7 @@ transactions.methods = {
           status: 'discarded',
           promoId: null,
         }}, (err, res) => { if(err) { throwError(err.message); } else {
-        }});  
+        }});
       }
     }
   }),
@@ -681,7 +681,11 @@ New on-demand order #${tx.orderNumber} in ${hab.name} for ${tx.company_name}. Re
 
 Meteor.methods({
   fetchMasterTransactions() {
-    return masterTransactions.find().fetch();
+    return masterTransactions.find({deliveryX: {$exists: true}}).fetch();
+  },
+  updateMasterTransactions(id, update) {
+    console.log("in meteor");
+    masterTransactions.update(id, update, (err) => {if (err) throwError(err)});
   },
   acceptOrder(id, method, role) {
       if(Meteor.isServer){
