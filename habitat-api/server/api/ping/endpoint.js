71ba2e18f4587d1dd0ac660b4d765e7bba269a1e
Mike Paszkiewicz <mike@tryhabitat.com>
2017-06-15 10:59:03 -0400

    api endpoins for habitats and health

---
+++ habitat-api/server/api/ping/endpoint.js
@@ -0,0 +1,10 @@
+Router.route( '/api/v1/ping', function() {
+  this.response.setHeader( 'Access-Control-Allow-Origin', '*' );
+  if ( this.request.method === "OPTIONS" ) {
+    this.response.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
+    this.response.setHeader( 'Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS' );
+    this.response.end( 'Set OPTIONS.' );
+  } else {
+     API.handleRequest(this, 'ping', this.request.method);
+  }
+}, { where: 'server' } );
