2550ed650db7b1564bd28df0a2d6d8f356f9a443
Mike Paszkiewicz <mike@tryhabitat.com>
2017-08-22 16:27:58 -0400

    methods

--- habitat-transactions/methods.transactions.js
+++ habitat-transactions/methods.transactions.js
@@ -933,7 +933,6 @@ handleInitialVendorContact = (txId) => {
             headers: "",
           });
         } else {
-
           console.log(`dynamic import ${module.dynamicImport}`);
           if(module.dynamicImport){
             import('phaxio').then((Phaxio) => {
@@ -944,25 +943,14 @@ handleInitialVendorContact = (txId) => {
                 `+1${businessProfiles.findOne(transactions.findOne(txId).sellerId).faxPhone.toString()}`,
                 string_data: res.content,
                 string_data_type: 'html'
-
               }, (error, data) => {
-                if(error) {
-                  console.warn(`fax error`);
-                  Email.send({
-                    from: "fax@tryhabitat.com",
-                    to: Meteor.settings.devMode ? 'mike@tryhabitat.com' : 'info@tryhabitat.com',
-                    subject: "Fax Failure",
-                    text: JSON.stringify(error, null, 2),
-                  });
-                } else {
-                  console.log(`fax success`)
+                if(error) { console.warn(`fax error`, error.message); } else {
+                  console.log(`fax success`);
                 }
               });
-            } catch(e) {
-              console.log(e);
-            }
-          });
+            });
           }
+        }
       });
 			break;
 		case 'email':
